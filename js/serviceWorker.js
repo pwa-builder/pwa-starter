8d41837a543e62c1282477f26bb52611d96cdb99
Alex <cryptonit63@yandex.ru>
2023-03-19 21:54:19 +0400

    pwa-app

---
+++ js/serviceWorker.js
@@ -0,0 +1,14 @@
+javascript
+if ('serviceWorker' in navigator) 
+
+{
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+      
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
